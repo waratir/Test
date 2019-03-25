1430f657e47bb019411c83a41a9d1acef255db1b
waratir <zhan.demo@gmail.com>
2019-03-25 11:47:03 +0300

    Initial commit

---
+++ src/home/Home.java
@@ -0,0 +1,16 @@
+package home;
+
+import java.util.Scanner;
+
+public class Home {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Home1 name1 = new Home1();
+        name1.b = 100;
+        name1.c = 200;
+        System.out.println(name1.light());
+//
+    }
+
+}
+
---
+++ src/home/Home1.java
@@ -0,0 +1,14 @@
+package home;
+
+public class Home1 {
+    private int c ;
+    private int b ;
+
+    public Home1(int c, int w){
+        this.c = c;
+    }
+    public int light(){
+        int k = c*b;
+        return k;
+    }
+}
